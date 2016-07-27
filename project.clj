30627f749c81602958de8f92e9994101808d2b5b
Gavin Cui <clarkhillm@gmail.com>
2016-07-27 18:30:15 +0800

    noting ...

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"] [http-kit "2.1.18"]]
+  :dependencies [
+                  [org.clojure/clojure "1.8.0"]
+                  [http-kit "2.1.18"]
+                  [criterium "0.4.4"]
+                  ]
   :main ^:skip-aot ocean.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}

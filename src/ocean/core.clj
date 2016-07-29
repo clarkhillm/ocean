ae37fbd88b6482f35960c904ba813ad339fbaa78
Gavin Cui <clarkhillm@gmail.com>
2016-07-29 11:22:57 +0800

    删除一些无用代码。

--- src/ocean/core.clj
+++ src/ocean/core.clj
@@ -18,9 +18,6 @@
     (if (nil? (first x)) rs
       (recur (rest x) (do (conj rs {:p (http/get (str "http://" (first x)) options),:ip (first x)}))))))
 
-
-;  (map (fn [x] {:p (http/get (str "http://" x) options),:ip x}) ips))
-
 (defn get_result [pms] (map (fn [x] (let [{:keys [opts error]} @(:p x)] (if error "-" (:ip x)))) pms))
 
 (defn test_main3 [ips] (map (fn [x] (get_result (generate_promise x))) ips))
